011211e70f6f8161c68a82fd81f24da6c959d211
KunKimlong <kimlongkun2412@gmail.com>
2025-04-19 16:57:46 +0700

    Login Line

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Redirect to LINE login
+Route::get('/login/line', function () {
+    return Socialite::driver('line')->redirect();
+});
+
+// Callback from LINE
+Route::get('/login/line/callback', function () {
+    $lineUser = Socialite::driver('line')->user();
+
+    $user = User::firstOrCreate(
+        ['line_id' => $lineUser->getId()],
+        [
+            'name' => $lineUser->getName(),
+            'email' => $lineUser->getEmail() ?? $lineUser->getId() . '@line.local', // LINE may not return email
+        ]
+    );
+
+    Auth::login($user);
+
+    return redirect('/home');
 });
